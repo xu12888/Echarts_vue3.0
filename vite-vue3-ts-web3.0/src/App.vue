ce30e326ff75ced9ec75f4746dcc45e355a8972f
xujie <384977866@qq.com>
2022-11-03 20:45:02 +0800

    add icon or index

--- vite-vue3-ts-web3.0/src/App.vue
+++ vite-vue3-ts-web3.0/src/App.vue
@@ -1,45 +1,181 @@
 <template>
-  <div class="aaa">
-    <el-menu :default-active="activeIndex2" class="el-menu-demo" mode="horizontal" background-color="red"
-      text-color="#fff" active-text-color="#ffd04b" @select="handleSelect">
+  <div class="Ov_back">
+    <!-- <顶部菜单> -->
+    <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" :ellipsis="false"
+      @select="handleSelect">
+      <el-menu-item index="0" style="margin-left: 10%;">ECHARTS</el-menu-item>
+      <div class="flex-grow" />
       <el-menu-item index="1">Processing Center</el-menu-item>
       <el-sub-menu index="2">
         <template #title>Workspace</template>
         <el-menu-item index="2-1">item one</el-menu-item>
         <el-menu-item index="2-2">item two</el-menu-item>
         <el-menu-item index="2-3">item three</el-menu-item>
-        <el-sub-menu index="2-4">
-          <template #title>item four</template>
-          <el-menu-item index="2-4-1">item one</el-menu-item>
-          <el-menu-item index="2-4-2">item two</el-menu-item>
-          <el-menu-item index="2-4-3">item three</el-menu-item>
-        </el-sub-menu>
       </el-sub-menu>
-      <el-menu-item index="3">ddddd</el-menu-item>
-      <el-menu-item index="4">Orders</el-menu-item>
     </el-menu>
+    <!-- <侧边菜单> -->
+    <el-row class="tac">
+      <el-col :span="2">
+        <el-menu default-active="2" class="el-menu-vertical-demo" @open="handleOpen" @close="handleClose">
+          <el-menu-item index="1">
+            <el-icon>
+              <icon-menu />
+            </el-icon>
+            <span>Navigator One</span>
+          </el-menu-item>
+          <el-menu-item index="2">
+            <el-icon>
+              <icon-menu />
+            </el-icon>
+            <span>Navigator Two</span>
+          </el-menu-item>
+          <el-menu-item index="3">
+            <el-icon>
+              <icon-menu />
+            </el-icon>
+            <span>Navigator Two</span>
+          </el-menu-item>
+          <el-menu-item index="4">
+            <el-icon>
+              <icon-menu />
+            </el-icon>
+            <span>Navigator Two</span>
+          </el-menu-item>
+          <el-menu-item index="5">
+            <el-icon>
+              <icon-menu />
+            </el-icon>
+            <span>Navigator Two</span>
+          </el-menu-item>
+          <el-menu-item index="6">
+            <el-icon>
+              <icon-menu />
+            </el-icon>
+            <span>Navigator Two</span>
+          </el-menu-item>
+          <el-menu-item index="7">
+            <el-icon>
+              <icon-menu />
+            </el-icon>
+            <span>Navigator Two</span>
+          </el-menu-item>
+          <el-menu-item index="8">
+            <el-icon>
+              <icon-menu />
+            </el-icon>
+            <span>Navigator Two</span>
+          </el-menu-item>
+          <el-menu-item index="9">
+            <el-icon>
+              <icon-menu />
+            </el-icon>
+            <span>Navigator Two</span>
+          </el-menu-item>
+          <el-menu-item index="10">
+            <el-icon>
+              <icon-menu />
+            </el-icon>
+            <span>Navigator Two</span>
+          </el-menu-item>
+          <el-menu-item index="11">
+            <el-icon>
+              <icon-menu />
+            </el-icon>
+            <span>Navigator Two</span>
+          </el-menu-item>
+          <el-menu-item index="12">
+            <el-icon>
+              <icon-menu />
+            </el-icon>
+            <span>Navigator Two</span>
+          </el-menu-item>
+          <el-menu-item index="13">
+            <el-icon>
+              <icon-menu />
+            </el-icon>
+            <span>Navigator Two</span>
+          </el-menu-item>
+          <el-menu-item index="14">
+            <el-icon>
+              <icon-menu />
+            </el-icon>
+            <span>Navigator Two</span>
+          </el-menu-item>
+          <el-menu-item index="15">
+            <el-icon>
+              <icon-menu />
+            </el-icon>
+            <span>Navigator Two</span>
+          </el-menu-item>
+          <el-menu-item index="16">
+            <el-icon>
+              <icon-menu />
+            </el-icon>
+            <span>Navigator Two</span>
+          </el-menu-item>
+          <el-menu-item index="17">
+            <el-icon>
+              <icon-menu />
+            </el-icon>
+            <span>Navigator Two</span>
+          </el-menu-item>
+          <el-menu-item index="18">
+            <el-icon>
+              <icon-menu />
+            </el-icon>
+            <span>Navigator Two</span>
+          </el-menu-item>
+          <el-menu-item index="19">
+            <el-icon>
+              <icon-menu />
+            </el-icon>
+            <span>Navigator Two</span>
+          </el-menu-item>
+          <el-menu-item index="20">
+            <el-icon>
+              <setting />
+            </el-icon>
+            <span>Navigator 03</span>
+          </el-menu-item>
+        </el-menu>
+      </el-col>
+    </el-row>
   </div>
+
 </template>
 
 <script lang="ts" setup>
-import { ref } from 'vue'
+import { ref } from 'vue';
+import {
+  Document,
+  Menu as IconMenu,
+  Location,
+  Setting,
+} from '@element-plus/icons-vue';
 
-const activeIndex2 = ref('1')
+const activeIndex = ref('1')
 const handleSelect = (key: string, keyPath: string[]) => {
   console.log(key, keyPath)
 }
-</script>
-<script setup lang="ts">
+const handleOpen = (key: string, keyPath: string[]) => {
+  console.log(key, keyPath)
+}
+const handleClose = (key: string, keyPath: string[]) => {
+  console.log(key, keyPath)
+}
 </script>
 <style scoped>
-.aaa {
+.Ov_back {
   width: 100%;
-  height: 100%;
-  background-color: black;
+  height: auto;
+  background-color: #fff;
 }
 
-.el-menu--horizontal {
-  border: none;
-  border-radius: 10px;
+.flex-grow {
+  flex-grow: 1;
+}
+
+.el-menu-demo {
+  background-color: rgba(255, 255, 255, 0.4);
 }
 </style>
--- vite-vue3-ts-web3.0/src/main.ts
+++ vite-vue3-ts-web3.0/src/main.ts
@@ -2,13 +2,18 @@ import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
 // todo 注入ui组件库
-import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
+import ElementPlus from "element-plus";
+import "element-plus/dist/index.css";
 import { store, key } from "./store";
+import * as ElementPlusIconsVue from "@element-plus/icons-vue";
 
 // createApp(App).mount("#app");
 
 const app = createApp(App);
 app.use(ElementPlus);
 app.use(store, key);
-app.mount('#app')
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component)
+  }
+app.mount("#app");
+
